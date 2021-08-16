e8382c281733fb962981590bcf8f8144335bf893
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2021-08-15 22:18:03 -0300

    chore: add RN Paper

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import HomeScreen from './src/screens/Home';
 
 export default function App() {
   return (
-    <>
+    <PaperProvider>
       <HomeScreen />
       <StatusBar style="auto" hidden={false} />
-    </>
+    </PaperProvider>
   );
 }
